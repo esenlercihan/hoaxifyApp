2de80dc89a0338c8cd435c1d466837627397aa32
Cihan Esenler <bm.cihan.esenler@gmail.com>
2021-09-04 12:42:11 +0300

    saving user to database and code refactoring

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -1,12 +1,23 @@
 package com.hoaxify.ws.user;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GeneratorType;
+
 import lombok.Data;
 
 //getter setter ve toString metodlarını hazirlamak/
 //kod kalabaligi olusturmak yerine Lombokun @Data anotasyonunu kullanabiliriz.
 @Data
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue
+	private long id;
 	private String username;
 	private String displayName;
 	private String password;
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -2,19 +2,29 @@ package com.hoaxify.ws.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hoaxify.ws.util.GenericResponse;
 
 @RestController
 public class UserController {
 	
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	
+	@Autowired
+	UserService userService;
 
 	@PostMapping("/api/1.0/users")
-	public void createUser(@RequestBody User user) {
+	@ResponseStatus(code = HttpStatus.OK)
+	public GenericResponse createUser(@RequestBody User user) {
 		log.info(user.toString());
+		userService.save(user);
+		return new GenericResponse("User Created");
 	}
 	
 }
