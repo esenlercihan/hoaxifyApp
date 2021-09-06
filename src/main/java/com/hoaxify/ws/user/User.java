469d95dc3bc2464fb942b6f85fbb5d3f0f836f22
Cihan Esenler <bm.cihan.esenler@gmail.com>
2021-09-06 20:42:05 +0300

    insert data controls and modifications

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -1,11 +1,13 @@
 package com.hoaxify.ws.user;
 
-import javax.annotation.Generated;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
-import org.hibernate.annotations.GeneratorType;
 
 import lombok.Data;
 
@@ -18,8 +20,19 @@ public class User {
 	@Id
 	@GeneratedValue
 	private long id;
+	
+	@NotNull
+	@Size(min = 4, max = 255)//hibernate aksini belirtmedigimiz surece string alanin degerini default olarak varchar(255) belirler.
+	@Column(unique = true)
 	private String username;
+	
+	@NotNull
+	@Size(min = 4, max = 255)
 	private String displayName;
+	
+	@NotNull
+	@Size(min = 8, max = 255)
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
 	private String password;
 	
 }
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,14 +1,25 @@
 package com.hoaxify.ws.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.util.GenericResponse;
 
 @RestController
@@ -21,10 +32,32 @@ public class UserController {
 
 	@PostMapping("/api/1.0/users")
 	@ResponseStatus(code = HttpStatus.OK)
-	public GenericResponse createUser(@RequestBody User user) {
+	public GenericResponse createUser(@Valid @RequestBody User user) {
 		log.info(user.toString());
 		userService.save(user);
 		return new GenericResponse("User Created");
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiError handleValidationException(MethodArgumentNotValidException exception) {
+		ApiError error = new ApiError(400, "Validation Error", "/api/1.0/users");
+		Map<String, String> validationErrors = new HashMap<>();
+		for(FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		error.setValidationErrors(validationErrors);
+		return error;
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiError handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
+		ApiError error = new ApiError(400, "Validation Error", "/api/1.0/users");
+		Map<String, String> validationErrors = new HashMap<>();
+		validationErrors.put("username", "Username must be unique");
+		error.setValidationErrors(validationErrors);
+		return error;
 	}
 	
 }
